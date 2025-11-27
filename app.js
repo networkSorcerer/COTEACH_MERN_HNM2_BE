c96e1e6030d99977bd725938a5b6b3fb57440e1d
networkSorcerer <gigattfyhig@gmail.com>
2025-11-27 21:03:51 +0900

    server on

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
-require(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.log("DB connection fail", err));
 
-const mongoURI = process.env.
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on");
+});
