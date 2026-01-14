d17819ee3aa9bbb9b1910462e3dc17ab5b14b761
networkSorcerer <gigattfyhig@gmail.com>
2026-01-14 09:51:54 +0900

    be

--- routes/user.api.js
+++ routes/user.api.js
@@ -3,5 +3,5 @@ const userController = require("../controllers/user.cotroller");
 const router = express.Router();
 
 router.post("/", userController.createUser);
-
+router.get("/me", authController.authenticate, userController.getUser);
 module.exports = router;
