30e5c3c79ee7dd0bb0324465e465b87695763d04
networkSorcerer <gigattfyhig@gmail.com>
2026-03-23 19:46:32 +0900

    be

--- routes/product.api.js
+++ routes/product.api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
 const router = express.Router();
-
+const productController = require("../controllers/product.controller");
 router.post(
   "/",
 
