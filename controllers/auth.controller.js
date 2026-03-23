86a0ba736d64fd3ff6e61a8c789d9ce47d4b1f40
networkSorcerer <gigattfyhig@gmail.com>
2026-03-23 19:38:03 +0900

    be

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -36,4 +36,14 @@ authController.authenticate = async (req, res, next) => {
   }
 };
 
+authController.checkAdminPermission = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (user.level !== "admin") throw new Error("no permission");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = authController;
